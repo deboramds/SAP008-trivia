3d1cab794d0181c82b529e5df6fa72afd5d82d57
deboramds <deborah.mart@hotmail.com>
2023-06-09 23:24:06 -0300

    função-somar-pontos-e-efeito-teclado

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
 /*
   Aqui vai o JavaScript
 */
+document.getElementsByClassName('container')[0].style.display = "block";
+
+
+function next(id){
+  document.getElementsByClassName('container')[id-1].style.display = "none";
+  document.getElementsByClassName('container')[id].style.display = "block";
+}
+
+function result(){
+  var score = 0;
+  if(document.getElementById('correct1').checked) {
+    score++;
+  }
+  if(document.getElementById('correct2').checked) {
+    score++;
+  }
+  if(document.getElementById('correct3').checked) {
+    score++;
+  }
+  alert("Sua pontuação é :"+score);
+}
